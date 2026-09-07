d56c910db599b6e52385666d6508169f1d384663
Rebel <agundadzidcoeva6530@gmail.com>
2026-09-07 21:28:46 +0300

    Добавьте файлы проекта.

---
+++ MyStructureData/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace MyStructureData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList myList = new MyList();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Insert(3, 2);
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.Write(myList[i] + " ");
+            }
+        }
+    }
+    class MyList
+    {
+        public int[] massive { get; set; } = new int[4];
+        public int Count { get; private set; } = 0;
+
+        public void Add(int value)
+        {
+
+            if (Count == massive.Length)
+            {
+                int[] newMassive = new int[massive.Length * 2];
+                for (int i = 0; i < massive.Length; i++)
+                {
+                    newMassive[i] = massive[i];
+                }
+                massive = newMassive;
+            }
+            massive[Count] = value;
+            Count++;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= massive.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return massive[index];
+            }
+            set
+            {
+                if (index < 0 || index >= massive.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                massive[index] = value;
+            }
+        }
+        public void Remove(int item)
+        {
+            for (int i = 0; i < massive.Length; i++)
+            {
+                if (massive[i] == item)
+                {
+                    for (int j = i; j < massive.Length - 1; j++)
+                    {
+                        massive[j] = massive[j + 1];
+
+                    }
+                    Count--;
+                }
+
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= massive.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            for (int i = 0; i < massive.Length; i++)
+            {
+                if (i == index)
+                {
+                    for (int j = i; j < massive.Length - 1; j++)
+                    {
+                        massive[j] = massive[j + 1];
+
+                    }
+                    Count--;
+                }
+            }
+        }
+        public void Insert(int index, int value)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (massive.Length < Count+1)
+            {
+                int[] newMassive = new int[massive.Length * 2];
+                for (int i = 0; i < massive.Length; i++)
+                {
+                    newMassive[i] = massive[i];
+                }
+                massive = newMassive;
+            }
+
+            for (int j = Count - 1; j >= index; j--)
+            {
+                massive[j + 1] = massive[j];
+
+            }
+            massive[index] = value;
+            Count++;
+
+        }
+        public void Clear()
+        {
+            Count = 0;
+        }
+    }
+}
